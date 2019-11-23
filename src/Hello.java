18152bd3ddf4485809852dfea266dcdd51076b35
AniaK9696 <kozun@wszib.edu.pl>
2019-11-23 16:10:26 +0100

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" What's  ur name?");
+        String name = scan.nextLine();
+
+        System.out.println("Hello " + name+"!");
+    }
+}
